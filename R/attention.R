fa60774c0edfc152c6f17c7724f86d18df77b4d0
Bastiaan Quast <bquast@gmail.com>
2022-06-23 15:09:46 +0200

    add basic R file and documentation for the package as a whole

---
+++ R/attention.R
@@ -0,0 +1,7 @@
+#' @name attention
+#' @title Self-Attention Algorithm
+#' @author Bastiaan Quast \email{bquast@@gmail.com}
+#' @description Implements functions for the Self-Attention algorithm, and demonstrates with vignettes of increasing depth how to construct the Self-Attention algorithm..
+#' @seealso https://qua.st/attention
+#' @references {https://qua.st/attention}
+NULL
