ddb2a6f612009aff0f79c34ec87d76f30f3f43fe
Bastiaan Quast <bquast@gmail.com>
2022-06-23 06:24:31 +0200

    compute the RowMaxs inside ComputeWeights

--- R/ComputeWeights.R
+++ R/ComputeWeights.R
@@ -8,6 +8,7 @@
 
 ComputeWeights <- function(scores, K) {
   size = dim(scores)[1]
+  maxs = RowMax(scores)
   weights = matrix(0, nrow=4, ncol=4)
   for (i in 1:size) {
     weights[i,] = SoftMax((scores[i,]-maxs[i,]) / ncol(K) ^ 0.5)
