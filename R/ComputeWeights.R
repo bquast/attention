93914c86563205a54c632a14bd8e28ba2ae76d6e
Bastiaan Quast <bquast@gmail.com>
2022-06-24 09:44:06 +0200

    use automatic detection of K instead of input variable

--- R/ComputeWeights.R
+++ R/ComputeWeights.R
@@ -1,17 +1,16 @@
 #' @name ComputeWeights
 #' @title SoftMax sigmoid function
 #' @param scores input value (numeric)
-#' @param K Key values matrix (numeric)
 #' @return output value (numeric)
 #' @export
 #'
 
-ComputeWeights <- function(scores, K) {
+ComputeWeights <- function(scores) {
   size = dim(scores)[1]
   maxs = RowMax(scores)
   weights = matrix(0, nrow=4, ncol=4)
   for (i in 1:size) {
-    weights[i,] = SoftMax((scores[i,]-maxs[i,]) / ncol(K) ^ 0.5)
+    weights[i,] = SoftMax((scores[i,]-maxs[i,]) / size ^ 0.5)
   }
   return(weights)
 }
