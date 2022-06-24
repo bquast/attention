a423adeca5f536618d4a7d3d504bcdcc340bb301
Bastiaan Quast <bquast@gmail.com>
2022-06-24 10:08:37 +0200

    add example

--- R/RowMax.R
+++ R/RowMax.R
@@ -3,6 +3,27 @@
 #' @param x input value (numeric)
 #' @return output value (numeric)
 #' @export
+#' @examples
+#' # generate a matrix of integers (also works for floats)
+#' set.seed(0)
+#' M = matrix(floor(runif(9, min=0, max=3)),
+#'            nrow=3,
+#'            ncol=3)
+#' print(M)
+#'
+#' # this outputs
+#' #      [,1] [,2] [,3]
+#' # [1,]    2    1    2
+#' # [2,]    0    2    2
+#' # [3,]    1    0    1
+#'
+#' as.matrix( RowMax(M) )
+#'
+#' # this outputs
+#' #      [,1]
+#' # [1,]    2
+#' # [2,]    2
+#' # [3,]    1
 #'
 
 RowMax <- function(x){
